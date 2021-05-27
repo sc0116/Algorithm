b27d2830f8c416cf0c261d671f2aa85ba6b892cb
SeungCheol <ssc6839@gmail.com>
2021-05-27 22:27:38 +0900

    Silver 1

---
+++ DynamicProgramming/BOJ_15486_퇴사 2.py
@@ -0,0 +1,10 @@
+n = int(input())
+tp = [list(map(int, input().split())) for _ in range(n)]
+dp = [0] * (n + 1)
+answer = 0
+for i in range(n):
+    answer = max(answer, dp[i])
+    if i + tp[i][0] > n:
+        continue
+    dp[i + tp[i][0]] = max(answer + tp[i][1], dp[i + tp[i][0]])
+print(max(dp))
