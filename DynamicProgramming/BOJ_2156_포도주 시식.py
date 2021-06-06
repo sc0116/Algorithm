a018bedcb4bb5e8c1dade42ddda03d839dc3a539
SeungCheol <ssc6839@gmail.com>
2021-06-06 19:26:52 +0900

    Silver 1

--- DynamicProgramming/BOJ_2156_포도주 시식.py
+++ DynamicProgramming/BOJ_2156_포도주 시식.py
@@ -11,4 +11,4 @@ else:
     dp = [0, arr[1], arr[1] + arr[2]] + [0] * (n - 2)
     for i in range(3, n + 1):
         dp[i] = max(dp[i - 1], dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i])
-    print(dp[i])
+    print(dp[n])
