a194a1a405b5fd962c28aaeae287ae4dedb82c69
SeungCheol <ssc6839@gmail.com>
2022-01-25 16:33:20 +0900

    feat: 정수 제곱근 판별

---
+++ Programmers/level 1/정수 제곱근 판별.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(long n) {
+        long sqrt = (long) Math.sqrt(n);
+
+        if (n == Math.pow(sqrt, 2)) {
+            return (long) Math.pow(sqrt + 1, 2);
+        }
+        return -1;
+    }
+}
