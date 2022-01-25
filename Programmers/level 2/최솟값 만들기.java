84e4b0909385dc658e579df5e947c001006b5c1c
SeungCheol <ssc6839@gmail.com>
2022-01-25 16:41:01 +0900

    feat: 최솟값 만들기

---
+++ Programmers/level 2/최솟값 만들기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int []A, int []B) {
+        int answer = 0;
+        int size = A.length;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0; i < size; i++) {
+            answer += A[i] * B[size - i - 1];
+        }
+
+        return answer;
+    }
+}
