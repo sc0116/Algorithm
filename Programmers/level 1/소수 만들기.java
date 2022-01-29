e8123048fca4e26c70c3b9cc7eacec6f3e7e074f
SeungCheol <ssc6839@gmail.com>
2022-01-29 19:26:24 +0900

    feat: 소수 만들기

---
+++ Programmers/level 1/소수 만들기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sosu(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean sosu(int n) {
+        int sqrt = (int) Math.sqrt(n);
+
+        for (int i = 2; i <= sqrt; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
