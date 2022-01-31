7f3e7fb6550229cd21248876c7a3022b0207f7d9
SeungCheol <ssc6839@gmail.com>
2022-01-31 16:49:39 +0900

    feat: K번째수

---
+++ Programmers/level 1/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1];
+            int idx = commands[i][2] - 1;
+
+            int[] subArray = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(subArray);
+
+            answer[i] = subArray[idx];
+        }
+
+        return answer;
+    }
+}
