cd6b5d3b729eff50e19ce859267b2641ac30f41f
SeungCheol <ssc6839@gmail.com>
2022-02-06 20:57:10 +0900

    feat: n^2 배열 자르기

---
+++ Programmers/level 2/n^2 배열 자르기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right - left) + 1];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = Math.max((int)((left + i) / n), (int)((left + i) % n)) + 1;
+        }
+
+        return answer;
+    }
+}
